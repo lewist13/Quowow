0ac6a10bbf4079f981a4bf66b2e79f5999aea938
lewist13 <lewistpro@gmail.com>
2021-05-20 17:01:44 -0400

    styled quowowbox and added foundational functionality

--- quowow-app/src/components/QuowowBox.js
+++ quowow-app/src/components/QuowowBox.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "../styles/QuowowBox.css";
+import { Avatar } from "@material-ui/core";
 
 function QuowowBox() {
-  return <div>yuh</div>;
+  return (
+    <div className="quowowBox">
+      <div className="quowowBox__info">
+        <Avatar />
+        <h5>Username</h5>
+      </div>
+      <div className="quowowBox__quowow">
+        <p>What is your question?</p>
+      </div>
+    </div>
+  );
 }
 
 export default QuowowBox;
