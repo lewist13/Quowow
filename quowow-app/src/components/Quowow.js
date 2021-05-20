2fb636922dd7d0bef137544eebacf8002cbe9ae7
lewist13 <lewistpro@gmail.com>
2021-05-20 16:12:04 -0400

    created sidebar component w/stylesheet; rendered it in quowow

--- quowow-app/src/components/Quowow.js
+++ quowow-app/src/components/Quowow.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
 import "../styles/Quowow.css";
 
 function Quowow() {
   return (
     <div className="quowow">
       <Navbar />
+      <div className="quowow__content">
+        <Sidebar />
+      </div>
     </div>
   );
 }
